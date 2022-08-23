3c6faadb41446e140553a386228f145b90dface4
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2022-08-23 12:50:38 +0200

    fix routines

--- YouNow Chat 2.0.user.js
+++ YouNow Chat 2.0.user.js
@@ -25,42 +25,86 @@
         });
     }
 
-
-    const startScanRoutineForNewUserName = function ()
+    function assignReferees(broadcastId, existingReferees, moderatorUserId, userId)
     {
-        console.log('scan for routine created');
-        setInterval(async function ()
-        {
-            if (!document.getElementById('iFrameThing'))
-                return;
 
-            if (lastName !== document.URL.replaceAll('https://www.younow.com/', ''))
-            {
-                lastName = document.URL.replaceAll('https://www.younow.com/', '');
-                userName = lastName;
-                userId = await getUserId(userName);
-                console.log('new User set!', userName, userId);
-                document.getElementById('iFrameThing').src = 'https://streamnow.pro/popupchat?cid=' + userId;
-            }
-
-        }, 1000);
-    };
-
+        fetch("//api.younow.com/php/api/doAdminAction", {
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                "x-requested-by": localStorage.getItem("requestBy")
+            },
+            "body": "actionId=" + '10' + "&userId=" + userId + "&onUserId=" + moderatorUserId + "&broadcastId=" + broadcastId + "&broadcaster=0",
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        });
+    }
 
     function checkIfReadyForManipulation(callback)
     {
-        console.log('check for manip created');
-        let interval = noDelaySetInterval(() =>
+        let interval = setInterval(() =>
         {
-            console.log('checking for chat-list');
             if (document.getElementsByClassName("chat-list").length !== 0)
             {
-                console.log('chat list found: cleared and starts function');
                 clearInterval(interval);
                 callback();
             }
         }, 1000)
     }
+
+    const checkForNewStream = function ()
+    {
+        let userName = '';
+        let lastName = '';
+        let userId = '';
+
+        noDelaySetInterval(async function ()
+        {
+            userName = document.URL.replaceAll('https://www.younow.com/', '');
+
+            console.log('Checking for new Stream...');
+            //If we are on the explore page, we return.
+            if (document.URL.includes('/explore'))
+                return;
+
+            //If we are on some empty page:
+            if ('' === document.URL.replaceAll('https://www.younow.com/', ''))
+                return;
+
+            //If we already triggered the event for the same page, we can go back.
+            if (lastName === userName)
+                return;
+
+            //We now know that we are on a Page and its a new Page so we get the data needed:
+            lastName = document.URL.replaceAll('https://www.younow.com/', '');
+            userName = lastName;
+            userId = await getUserId(userName);
+            console.log('New User set!', userName, userId);
+
+            //If the iFrame is already created, like switching from one stream we just change the iFrame src:
+            if (document.getElementById('iFrameThing'))
+            {
+                console.log('iFrame was already created, so we just change url.');
+                document.getElementById('iFrameThing').src = 'https://streamnow.pro/popupchat?cid=' + userId;
+                return;
+            }
+
+            console.log('No iFrame found, waiting for chat to load.')
+            checkIfReadyForManipulation(function ()
+            {
+                console.log('Chat found, creating iFrame');
+                let div = document.getElementsByClassName("chat-list");
+                let iFrameElem = document.createElement('iframe');
+                iFrameElem.id = 'iFrameThing';
+                iFrameElem.style.height = '100%';
+                iFrameElem.style.width = '100%';
+                iFrameElem.src = 'https://streamnow.pro/popupchat?cid=' + userId;
+                iFrameElem.classList = div[0].parentNode.classList;
+                document.getElementsByClassName('chat-list')[0].append(iFrameElem);
+            })
+
+        }, 1000);
+    };
 
     function noDelaySetInterval(func, interval)
     {
@@ -68,34 +112,29 @@
         return setInterval(func, interval);
     }
 
-    function sleep(delay)
+    function addParentListener()
     {
-        var start = new Date().getTime();
-        while (new Date().getTime() < start + delay) ;
+        window.addEventListener('message', function (e)
+        {
+            // Get the sent data
+            const data = e.data;
+            const test = document.querySelectorAll('[title="' + data + '"][class="truncate ng-star-inserted"]');
+            if (test.length === 0)
+            {
+                return;
+            } else
+                test[0].click();
+
+            console.log(data);
+
+            // If you encode the message in JSON before sending them,
+            // then decode here
+            // const decoded = JSON.parse(data);
+        });
     }
 
-
-    console.log('Running Chat');
-    let lastName;
-    let userName = document.URL.replaceAll('https://www.younow.com/', '');
-    let userId = await getUserId(userName);
-
-    lastName = userName;
-
-    console.log(userName, userId);
-
-    checkIfReadyForManipulation(function ()
+    function removeNormalChat()
     {
-        console.log('change executed');
-        let div = document.getElementsByClassName("chat-list");
-        let iFrameElem = document.createElement('iframe');
-        iFrameElem.id = 'iFrameThing';
-        iFrameElem.style.height = '100%';
-        iFrameElem.style.width = '100%';
-        console.log(userId);
-        iFrameElem.src = 'https://streamnow.pro/popupchat?cid=' + userId;
-        iFrameElem.classList = div[0].parentNode.classList;
-        document.getElementsByClassName('chat-list')[0].append(iFrameElem);
         let styles = `.comment-wrapper
                 {
                           display:none;
@@ -105,25 +144,16 @@
         styleSheet.type = "text/css"
         styleSheet.innerText = styles
         document.head.appendChild(styleSheet)
+    }
 
-    })
-    startScanRoutineForNewUserName();
-
-    window.addEventListener('message', function (e)
+    function runCode()
     {
-        // Get the sent data
-        const data = e.data;
-        const test = document.querySelectorAll('[title="' + data + '"][class="truncate ng-star-inserted"]');
-        if (test.length === 0)
-        {
-            return;
-        } else
-            test[0].click();
+        console.log('Start Chat Script...');
+        removeNormalChat();
+        addParentListener();
+        checkForNewStream();
+    }
 
-        console.log(data);
+    runCode();
 
-        // If you encode the message in JSON before sending them,
-        // then decode here
-        // const decoded = JSON.parse(data);
-    });
 })();
