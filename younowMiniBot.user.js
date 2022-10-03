1ca3e17e1f6e30fd4365f3911fc9f8c3f2402925
lucalazzeri97@gmail.com <lucalazzeri97@gmail.com>
2022-10-03 14:05:12 +0200

    update script for eli

--- younowMiniBot.user.js
+++ younowMiniBot.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Mini commands chat bot
+// @name         Yonow Chat 2.0
 // @namespace    https://streamnow.pro
 // @version      0.1
-// @description  A queue system I use for the pelinka games
+// @description  Integrates the Streamnow Chat into Younow
 // @author       Lazzeri
 // @match        https://www.younow.com/*
 // @grant        none
@@ -12,27 +12,10 @@
 
 (async function ()
 {
+    const MESSAGE = 'Message';
+    const TIMETOWAITINSECONDS = 30;
+    const publicUserId = 33446030;
     'use strict';
-    let local = 'en';
-    const userName = '123FreshLikeMe';
-    const commands = {
-        discord: 'The discord message',
-        otherone: '',
-        otherone2: ''
-    }
-
-    //Helper Functions ---------------------------------------------------
-    function sleep(milliseconds)
-    {
-        if (milliseconds < 0)
-            return new Promise(resolve =>
-            {
-                resolve()
-            })
-
-        return new Promise(resolve => setTimeout(resolve, milliseconds));
-    }
-
 
     function getUserInfo(userName)
     {
@@ -45,14 +28,25 @@
         });
     }
 
-    const sendMessage = (userId, text) =>
+    function sleep(milliseconds)
+    {
+        if (milliseconds < 0)
+            return new Promise(resolve =>
+            {
+                resolve()
+            })
+
+        return new Promise(resolve => setTimeout(resolve, milliseconds));
+    }
+
+    function assignReferees(userId, channelId, comment)
     {
         fetch("//api.younow.com/php/api/broadcast/chat", {
             "headers": {
                 "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
                 "x-requested-by": localStorage.getItem("requestBy")
             },
-            "body": "userId=" + userId + "&channelId=" + userId + "&comment=" + text + "&tsi=qTARYFhKsb&tdi=tV16GrJcrS&lang=" + local,
+            "body": "userId=" + userId + "&channelId=" + channelId + "&comment=" + comment + "&tsi=qTARYFhKsb&tdi=tV16GrJcrS&lang=es",
             "method": "POST",
             "mode": "cors",
             "credentials": "include"
@@ -60,76 +54,16 @@
     }
 
 
-    const setupPusher = (userId, giftCallback) =>
-    {
-        const installPusherLibrary = () =>
-        {
-            return new Promise(async resolve =>
-            {
-                console.log('Installing Pusher Library')
-                let head = document.getElementsByTagName('head')[0];
-                let link = document.createElement('script');
-                link.src = 'https://js.pusher.com/7.0/pusher.min.js';
-                head.appendChild(link);
-                await sleep(2000);
-                return resolve();
-            })
-        }
-
-        const connectToWebsocket = (callBack, userId) =>
-        {
-            console.log('Connecting to websocket');
-            let pusher = new Pusher('d5b7447226fc2cd78dbb', {
-                cluster: "younow"
-            });
-            console.log('Used userid:', userId);
-
-            let channel = pusher.subscribe("public-channel_" + userId);
-
-            channel.bind('onChat', function (data)
-            {
-                for (let i = 0; i < data.message.comments.length; i++)
-                {
-                    let obj = data.message.comments[i];
-                    callBack(obj)
-                }
-            });
-        }
-
-        installPusherLibrary().then(() => connectToWebsocket(giftCallback, userId));
-        return 'Done';
-    }
-
-    const triggerEvent = (name, text, userId) =>
-    {
-        for (const [command, message] of Object.entries(commands))
-        {
-            if (text.toLowerCase().includes(command.toLowerCase()))
-            {
-                sendMessage(userId, '@' + name + ' ' + message);
-            }
-        }
-    }
-
-    //MAIN ----------------------------------------------------
     async function runCode()
     {
-        const {userId} = await getUserInfo(userName);
-        setupPusher(userId, async (data) =>
+        console.log('Lets go!');
+        setInterval(() =>
         {
-            //Ignore own comments
-            if(parseInt(data.userId) === parseInt(userId))
-                return;
-
-            triggerEvent(data.name, data.comment, userId);
-        })
+            assignReferees(publicUserId, publicUserId, MESSAGE);
+        }, TIMETOWAITINSECONDS * 1000)
+        await sleep(5000);
     }
 
     runCode();
 
 })();
-
-
-
-
-
